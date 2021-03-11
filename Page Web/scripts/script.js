7e3bdb2103542cc3ee111fc2663b348e3a3b3e30
Sapae Safae <sapaebaamel@gmail.com>
2021-03-11 10:56:18 +0100

    update

--- Page Web/scripts/script.js
+++ Page Web/scripts/script.js
@@ -5,3 +5,26 @@ $(document).ready(function() {
 
     });
 });
+
+
+const form = document.getElementById('form')
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const errorElement = document.getElementById('error')
+const message = document.getElementById('message')
+
+form.addEventListener('submit', (e) => {
+    let messages  = []
+    if (name.value == '' || name.value == null) {
+        messages.push('Name is required')
+    }
+    if (message.length < 6) {
+        prompt('Password mush be longer than 6 characters')
+    }
+
+    if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+    }
+
+})
